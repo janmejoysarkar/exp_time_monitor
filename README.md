50ecbe603e15569f18ac32ce1cfa0d5d11689904
janmejoy <janmejoysarker@gmail.com>
2024-06-27 11:24:47 +0530

    README added. Code commented

---
+++ README.md
@@ -0,0 +1,23 @@
+
+![Logo](https://suit.iucaa.in/sites/default/files/top_banner_compressed_2_1.png)
+
+
+# CMD_EXPT and MEAS_EXP plots generator
+- 2k 2k images to be used to show both off limb and on limb structures simultaneously.
+- The project is to make PR images for SUIT.
+
+
+Created by Janmejoy Sarkar @ [SUIT](https://suit.iucaa.in)LAB \
+Initiated on Jun 26 2024
+
+## Authors
+
+- [@janmejoysarkar](https://github.com/janmejoysarkar)
+
+
+## Acknowledgements
+
+ - [IUCAA, Pune](https://www.iucaa.in)
+ - [ISRO, Aditya-L1](https://www.isro.gov.in/Aditya_L1.html)
+
+
--- src/exposure_time_monitor.py
+++ src/exposure_time_monitor.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun 26 14:53:37 2024
-
+-Created to generate monthly plots for CMD_EXPT and MEAS_EXP for all filters.
+-Discrepany was seen in the data for few days. So this was created to see the
+history.
 @author: janmejoyarch
 """
 import os, glob
@@ -11,6 +13,12 @@ import matplotlib.pyplot as plt
 from concurrent.futures import ProcessPoolExecutor
 
 def generator(ftr_name):
+    '''
+    Takes Filter Name as input.
+    Gives Plots as output.
+    'file' is the excel file fed by global variable.
+    Plots CMD_EXPT and MEAS_EXP for a month for the mentioned filter.
+    '''
     print(f"Running for {ftr_name}\t")
     cmd_expt_all, meas_exp_all, time_all = pd.Series(), pd.Series(), pd.Series()
     for file in files:
@@ -24,14 +32,13 @@ def generator(ftr_name):
             time_all= pd.concat([time_all, time], ignore_index=True)
         except KeyError:
             print("Skipped", file)
-        
     #plotting
     plt.figure()
     plt.plot(time_all, cmd_expt_all, '.', label="CMD_EXPT")
     plt.plot(time_all, meas_exp_all, '.', label="MEAS_EXPT")
     plt.title(f"Filter: {ftr_name} | Date: {year}_{month}")
     plt.yscale("log")
-    plt.ylabel("Exp Time (ms")
+    plt.ylabel("Exp Time (ms)")
     plt.legend()
     plt.grid()
     plt.xticks(rotation=30)
@@ -39,20 +46,23 @@ def generator(ftr_name):
     print(f"products/{ftr_name}_{year}_{month}.pdf Saved!")
     plt.show()
     
-    
 if __name__=="__main__":
-    ftr_list=["NB01","NB02","NB03","NB04","NB05","NB06","NB07","NB08","BB01", "BB02", "BB03", "BP02", "BP03", "BP04"]
+    ftr_list=["NB01","NB02","NB03","NB04","NB05","NB06","NB07","NB08","BB01", "BB02", "BB03"]
     SAVE=True
     project_path= os.path.realpath("..")
-    
     for year_dir in glob.glob(os.path.join(project_path, "data/raw/*")):
+    #List of years in the folder.
         year=os.path.basename(year_dir)
         print(year)
         for month_dir in glob.glob(os.path.join(year_dir,"*")):
-            if len(os.path.basename(month_dir)) < 3:
+        #List of months in a year's directory.
+            if len(os.path.basename(month_dir)) < 3: 
+            #To pick folders only.
+            #erroneous excel files to be excluded
                 month= os.path.basename(month_dir) 
             print(year, month)
-            files= glob.glob(os.path.join(month_dir, "*/*.xlsx"))
-            with ProcessPoolExecutor() as executor:
+            files= glob.glob(os.path.join(month_dir, "*/*.xlsx")) #list xlsx files
+            with ProcessPoolExecutor() as executor: 
+            #One processes runs for one filter for one month data.
                 executor.map(generator, ftr_list)
 
